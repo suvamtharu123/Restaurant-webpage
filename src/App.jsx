dbbd2d2ab3ee829351d22b6eb34f43a72c07abf9
Suvam Chaudhary <suvamraytharu76@gmail.com>
2024-03-04 11:47:26 +0545

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./Components/Navbar"
+import Hero from "./Components/Hero"
+import HeadlineCard from "./Components/HeadlineCard"
+import Food from "./Components/Food"
+import Category from "./Components/Category"
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HeadlineCard />
+      <Food />
+      <Category />
+    </>
+  )
+}
+
+export default App
